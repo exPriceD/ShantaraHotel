cffdbd620a860800cfee2e874db225d587922c35
Maxim <lolerpro4@gmail.com>
2024-04-26 21:04:58 +0300

    [feet]: Added the ability to select whether to output the time when accessing the get_current_date function

--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,16 @@ from datetime import datetime
 import pytz
 
 
-def get_current_time(timezone='Europe/Moscow'):
+def get_current_date(timezone='Europe/Moscow', return_time=True):
     moscow_tz = pytz.timezone(timezone)
     moscow_time = datetime.now(moscow_tz)
-    formatted_time = moscow_time.strftime('%d.%m.%y %H:%M')
-    return formatted_time
+    if return_time:
+        return moscow_time.strftime('%d.%m.%y %H:%M')
+    return moscow_time.strftime('%d.%m.%y')
+
+
+def format_date(date_obj):
+    # date_obj = datetime.strptime(date_str, '%d.%m.%y')
+    formatted_date = date_obj.strftime('20%y-%m-%dT00:00:00')
+
+    return formatted_date
